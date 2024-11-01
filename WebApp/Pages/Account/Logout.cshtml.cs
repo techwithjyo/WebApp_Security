b5d893362eac9a3142d6b1acc7f2404907522b64
jyotirmoy <jmaschatak1@gmail.com>
2024-11-01 12:59:39 +0530

    Logout functionality

---
+++ WebApp/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login");
+        }
+    }
+}
